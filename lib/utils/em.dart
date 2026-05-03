8beb5c339373a16ce6006c519435c80fa0c9d59d
azazo1 <azazo1@qq.com>
2026-05-03 18:47:03 +0800

    fix: title html decoding

--- lib/utils/em.dart
+++ lib/utils/em.dart
@@ -1,10 +1,23 @@
 abstract final class Em {
   static final _exp = RegExp('<[^>]*>([^<]*)</[^>]*>');
-  static final _htmlRegExp = RegExp(r'&(lt|gt|quot|apos|nbsp|amp);');
+  static final _htmlRegExp = RegExp(
+    r'&(#(?:\d+|x[\da-fA-F]+)|lt|gt|quot|apos|nbsp|amp);',
+  );
 
   static String regCate(String origin) {
     Iterable<Match> matches = _exp.allMatches(origin);
-    return matches.lastOrNull?.group(1) ?? origin;
+    return decodeHtml(matches.lastOrNull?.group(1) ?? origin);
+  }
+
+  static String decodeHtml(String origin) {
+    String result = origin;
+    while (true) {
+      final decoded = result.replaceAllMapped(_htmlRegExp, _decodeHtmlEntity);
+      if (decoded == result) {
+        return decoded;
+      }
+      result = decoded;
+    }
   }
 
   static List<({bool isEm, String text})> regTitle(String origin) {
@@ -20,23 +33,29 @@ abstract final class Em {
         if (str != '') {
           res.add((
             isEm: false,
-            text: str.replaceAllMapped(
-              _htmlRegExp,
-              (m) => switch (m.group(1)) {
-                'lt' => '<',
-                'gt' => '>',
-                'quot' => '"',
-                'apos' => "'",
-                'nbsp' => ' ',
-                'amp' => '&',
-                _ => m.group(0)!,
-              },
-            ),
+            text: decodeHtml(str),
           ));
         }
         return '';
       },
     );
     return res;
+  }
+
+  static String _decodeHtmlEntity(Match m) {
+    final entity = m.group(1)!;
+    final codePoint = switch (entity) {
+      'lt' => 60,
+      'gt' => 62,
+      'quot' => 34,
+      'apos' => 39,
+      'nbsp' => 32,
+      'amp' => 38,
+      _ when entity.startsWith('#x') =>
+        int.tryParse(entity.substring(2), radix: 16),
+      _ when entity.startsWith('#') => int.tryParse(entity.substring(1)),
+      _ => null,
+    };
+    return codePoint == null ? m.group(0)! : String.fromCharCode(codePoint);
   }
 }
